7c70aed6e3b8e95b5a93cb606d6de4b45a1c860e
y-murata <y-murata@oz-vision.co.jp>
2015-11-03 16:53:22 +0900

    [modify]コードをいじった

--- src/controller/__init__.py
+++ src/controller/__init__.py
@@ -2,9 +2,14 @@
 
 import os
 
-# html取得
-os.system("xargs -P 200 -n 1 wget --recursive --quiet --accept=html --wait=1 --random-wait --force-directories --directory-prefix=../../sites/ < ../../company.csv")
+with open("../work/company.csv") as f:
+    url = f.read()
 
-# 文字コード変換
-# os.system("nkf -Xw --overwrite /Users/muratayu/codebreak/sites/**/*.html")
-os.system("nkf -Xw --overwrite ../../sites/**/*.html")
+    # html取得
+    os.system("wget --recursive --quiet --accept=html --wait=1 --random-wait --force-directories --directory-prefix=../../sites/ " + url)
+
+    # 文字コード変換
+    os.system("nkf -Xw --overwrite ../../sites/**/*.html")
+
+    # 変更をコミット
+    os.system("git commit -a")
