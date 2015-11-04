4258f6a62a7079cd722523896cea17d2062d4de0
y-murata <y-murata@oz-vision.co.jp>
2015-11-04 21:39:18 +0900

    [modify]少し関数に分けた

--- src/controller/__init__.py
+++ src/controller/__init__.py
@@ -3,19 +3,32 @@
 import os
 from datetime import datetime
 
+
+def git_commit():
+    os.system("git add ../../sites/")
+    os.system("git commit -a -m 'auto commit : %s : %s'" % (url, nowDate))
+    os.system("git push origin master")
+
+
+def wget_list():
+    os.system("wget --recursive --quiet --accept=html --wait=1 --random-wait --force-directories --directory-prefix=../../sites/ " + url)
+
+
+def file_encode():
+    os.system("nkf -Xw --overwrite ../../sites/**/*.html")
+
+
 for url in open("../work/company.csv", "r"):
     print(url)
 
     # html取得
-    os.system("wget --recursive --quiet --accept=html --wait=1 --random-wait --force-directories --directory-prefix=../../sites/ " + url)
+    wget_list()
 
     # 文字コード変換
-    os.system("nkf -Xw --overwrite ../../sites/**/*.html")
+    file_encode()
 
     # 現在日時の取得
     nowDate = datetime.now().strftime("%Y/%m/%d")
 
     # wgetによる変更をプッシュ
-    os.system("git add ../../sites/")
-    os.system("git commit -a -m 'auto commit : %s : %s'" % (url, nowDate))
-    os.system("git push origin master")
+    git_commit()
