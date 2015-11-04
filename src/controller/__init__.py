ef805cd07f838bfc97022ed3dd5c2de2c84c30fb
y-murata <y-murata@oz-vision.co.jp>
2015-11-05 05:08:42 +0900

    [modify]関数化したけど戻す

--- src/controller/__init__.py
+++ src/controller/__init__.py
@@ -3,33 +3,19 @@
 import os
 from datetime import datetime
 
-
-def git_commit(url, nowDate):
-    os.system("git add ../../sites/")
-    os.system("git commit -a -m 'auto commit : %s : %s'" % (url, nowDate))
-    os.system("git push origin master")
-
-
-def wget_list():
-    os.system(
-        "wget --recursive --quiet --accept=html --wait=1 --random-wait --force-directories --directory-prefix=../../sites/ " + url)
-
-
-def file_encode():
-    os.system("nkf -Xw --overwrite ../../sites/**/*.html")
-
-
 for url in open("../work/company.csv", "r"):
     print(url)
 
     # html取得
-    wget_list()
+    os.system("wget --recursive --quiet --accept=html --wait=1 --random-wait --force-directories --directory-prefix=../../sites/ " + url)
 
     # 文字コード変換
-    file_encode()
+    os.system("nkf -Xw --overwrite ../../sites/**/*.html")
 
     # 現在日時の取得
-    nowDate = datetime.now().strftime("%Y/%m/%d_%H/%M/%S")
+    nowDate = datetime.now().strftime("%Y/%m/%d")
 
     # wgetによる変更をプッシュ
-    git_commit(url, nowDate)
+    os.system("git add ../../sites/")
+    os.system("git commit -a -m 'auto commit_%s_%s'" % (url, nowDate))
+    os.system("git push origin master")
