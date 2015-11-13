ceb7a4edb2521816cb62e11e112f920fec1affd7
y-murata <y-murata@oz-vision.co.jp>
2015-11-13 22:22:37 +0900

    [modify]コメントを削除

--- src/controller/__init__.py
+++ src/controller/__init__.py
@@ -33,17 +33,12 @@ if __name__ == '__main__':
         # # 会社毎の追加してコミットするようにすればいいんじゃね？
         os.system("git add ../../sites/")
         os.system("git commit -a -m 'auto commit_%s %s'" % (url, nowDate))
-        # os.system("git push -u origin %s" % branch)  # ローカルブランチ:リモートブランチ
-
-        # プルリクエスト : 要インストール : brew install hub
-        # os.system("hub pull-request -m auto_PR_%s" % url)
 
         # masterに移動，最新をプル
         os.system("git checkout master")
         os.system("git pull -u origin master")
 
         # マージする
-        ## マージするときはmasterのHEADに行ってマージしないといけない
         os.system("git merge %s --no-ff" % branch)
 
         # ブランチを削除する
